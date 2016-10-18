6a70a6b916b8d5f99a0338a16fb3faa0e203c100
Michael Strohmayer <michael.strohmayer@campus.lmu.de>
2016-10-18 16:12:35 +0200

    added setSectionStatus

--- snippetCollection.js
+++ snippetCollection.js
@@ -154,3 +154,39 @@ function changeForm(formid,formName) {
  *Notes:
  *          Tested for Microsoft Dynamics CRM 2016
  */
+
+
+function setSectionStatus (sectionName, status)
+{
+    try {
+        var ctrlName = Xrm.Page.ui.controls.get();
+        for (var i in ctrlName) {
+            var ctrl = ctrlName[i];
+            var ctrlSection = ctrl.getParent().getName();
+            if (ctrlSection == sectionName) {
+                ctrl.setDisabled(status);
+            }
+        }
+        return true;
+    }catch (err){
+        return (err.message);
+    }
+}
+
+
+/**
+ *Syntax:   setSectionStatus(sectionName,status)
+ *Parameter:
+ *          sectionName    =   String
+ *          status         =   Boolean
+ *
+ *Example:
+ *          setSectionStatus("Contacts",true);
+ *
+ *Return:
+ *          Success:    Returns true
+ *          Error:    Returns err.message
+ *
+ *Notes:
+ *          Tested for Microsoft Dynamics CRM 2016
+ */
