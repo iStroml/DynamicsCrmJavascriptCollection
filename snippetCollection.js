e6848fd495f507525ad8010d1d1d059ffd4a5fff
Michael Strohmayer <michael.strohmayer@campus.lmu.de>
2016-10-19 08:41:03 +0200

    added setFormStatus

--- snippetCollection.js
+++ snippetCollection.js
@@ -226,3 +226,40 @@ function setSectionStatus (sectionName, status)
  *Notes:
  *          Tested for Microsoft Dynamics CRM 2016
  */
+
+
+
+function setFormStatus(status)
+{
+    try {
+        var allAttributes = Xrm.Page.data.entity.attributes.get();
+        for (var i in allAttributes) {
+            var currentattribte = Xrm.Page.data.entity.attributes.get(allAttributes[i].getName());
+            var attributename = currentattribte.getName();
+            Xrm.Page.getControl(attributename).setDisabled(status);
+        }
+        return true;
+    }catch (err){
+        console.log(err.message);
+        return false;
+    }
+}
+
+
+/**
+ * Disables alll forms
+ *
+ *Syntax:   setFormStatus(status)
+ *Parameter:
+ *          status = Boolean
+ *
+ *Example:
+ *          setFormStatus(true);
+ *
+ *Return:
+ *          Success:    Returns true
+ *          Error:    Returns false
+ *
+ *Notes:
+ *          Tested for Microsoft Dynamics CRM 2016
+ */
