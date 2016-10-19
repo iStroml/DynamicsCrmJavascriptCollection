268142531ba9f6efbe3b6142c677bde7516f7a56
Michael Strohmayer <michael.strohmayer@campus.lmu.de>
2016-10-19 12:08:23 +0200

    added openPopupWindow

--- snippetCollection.js
+++ snippetCollection.js
@@ -263,3 +263,39 @@ function setFormStatus(status)
  *Notes:
  *          Tested for Microsoft Dynamics CRM 2016
  */
+
+
+
+function openPopupWindow(name, url, width,height) {
+    var feature = "status=1";
+    try {
+        openStdWin(url, name, width, height, feature);
+        return true;
+    }catch(err){
+        console.log(err.message);
+        return true;
+    }
+}
+
+
+
+/**
+ * Opens a popup window
+ *
+ *Syntax:   openPopupWindow(name, url, width,height)
+ *Parameter:
+ *          var url = String
+ *          var name = String
+ *          var width = Int
+ *          var height = Int
+ *
+ *Example:  openPopupWindow("popup", "https://possibleLink.crm4.dynamics.com/main.aspx#235127225", 800,600)
+
+ *
+ *Return:
+ *          Success:    Returns true
+ *          Error:    Returns false
+ *
+ *Notes:
+ *          Tested for Microsoft Dynamics CRM 2016
+ */
